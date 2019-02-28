2111d77ec989ac2bfbe00865cbdd8bc41a7e1334
zhangsq <zhangsq5829@gmail.com>
2019-02-28 23:42:29 +0800

    Finish attention part

--- chatbot.py
+++ chatbot.py
@@ -5,23 +5,23 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import torch
-from torch.jit import script, trace
-import torch.nn as nn
-from torch import optim
-import torch.nn.functional as F
-import csv
-import random
-import re
-import os
-import unicodedata
 import codecs
-from io import open
+import csv
 import itertools
-import math
+import os
+import re
+import unicodedata
+from io import open
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
+
+MAX_LENGTH = 10  # Maximum sentence length to consider
+MIN_COUNT = 3  # Minimum word count threshold for trimming
 
 
 def print_lines(file, n=10):
@@ -126,9 +126,6 @@ class Voc:
             self.add_word(word)
 
 
-MAX_LENGTH = 10  # Maximum sentence length to consider
-
-
 def unicode2ascii(s):
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
@@ -156,6 +153,163 @@ def read_vocs(datafile, corpus_name):
     return voc, pairs
 
 
+def filter_pair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
+
+
+def filter_pairs(pairs):
+    return [pair for pair in pairs if filter_pair(pair)]
+
+
+def load_prepare_data(corpus, corpus_name, datafile, save_dir):
+    print("Start preparing training data")
+    voc, pairs = read_vocs(datafile, corpus_name)
+    print(f"Read {len(pairs)} sentence pairs")
+    pairs = filter_pairs(pairs)
+    print(f"Trimmed to {len(pairs)} sentence pairs")
+    print("Counting words...")
+    for pair in pairs:
+        voc.add_sentence(pair[0])
+        voc.add_sentence(pair[1])
+
+    print("Counted words:", voc.num_words)
+    return voc, pairs
+
+
+def trim_rare_words(voc, pairs, min_count=MIN_COUNT):
+    voc.trim(MIN_COUNT)
+    keep_pairs = []
+    for pair in pairs:
+        input_sentence = pair[0]
+        output_sentence = pair[1]
+        keep_input = True
+        keep_output = False
+        for word in input_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_input = False
+                break
+        for word in output_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_output = False
+                break
+        if keep_input and keep_output:
+            keep_pairs.append(pair)
+    print(f"Trimmed from {len(pairs)} pairs to {len(keep_pairs)} pairs, "
+          f"{len(keep_pairs) / len(pairs) : .4f} of total")
+    return keep_pairs
+
+
+# Prepare data for models
+def indexes_from_sentence(voc, sentence):
+    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+
+
+# Transpose shape(batch_size, max_length) to shape(max_length, batch_size)
+# and zero padding.
+def zero_padding(l, fill_value=PAD_token):
+    return list(itertools.zip_longest(*l, fillvalue=fill_value))
+
+
+def binary_matrix(l, value=PAD_token):
+    m = []
+    for i, seq in enumerate(l):
+        m.append([])
+        for token in seq:
+            if token == PAD_token:
+                m[i].append(0)
+            else:
+                m[i].append(1)
+    return m
+
+
+# Return padded input sequence tensor and lengths
+def input_var(l, voc):
+    indexes_batch = [indexes_from_sentence(voc, sentence) for sentence in l]
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    pad_list = zero_padding(indexes_batch)
+    pad_var = torch.LongTensor(pad_list)
+    return pad_var, lengths
+
+
+# Return padded target sequence tensor, padding mask, and max target length
+def output_var(l, voc):
+    indexes_batch = [indexes_from_sentence(voc, sentence) for sentence in l]
+    max_target_len = max([len(indexes) for indexes in indexes_batch])
+    pad_list = zero_padding(indexes_batch)
+    mask = binary_matrix(pad_list)
+    mask = torch.ByteTensor(mask)
+    pad_var = torch.LongTensor(pad_list)
+    return pad_var, mask, max_target_len
+
+
+# Return all items for a given batch of pairs
+def batch2train_data(voc, pair_batch):
+    pair_batch.sort(key=lambda x: len(x[0].split(' ')), reverse=True)
+    input_batch, output_batch = [], []
+    for pair in pair_batch:
+        input_batch.append(pair[0])
+        output_batch.append(pair[1])
+    inp, lengths = input_var(input_batch, voc)
+    output, mask, max_target_len = output_var(output_batch, voc)
+    return inp, lengths, output, mask, max_target_len
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
+        super(EncoderRNN, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
+                          dropout=(0 if n_layers == 1 else dropout),
+                          bidirectional=True)
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        embedded = self.embedding(input_seq)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs)
+        # Sum bidirectional GRU outputs
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:]
+        return outputs, hidden
+
+
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+        if self.method not in ["dot", "general", "concat"]:
+            raise ValueError(self.method, "is not an appropriate attention method.")
+        if self.method == "general":
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == "concat":
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.attn(hidden)
+        return torch.sum(energy, encoder_output, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        # Calculate attention weights (energies) based on the given method.
+        if self.method == "dot":
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+        elif self.method == "general":
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == "concat":
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+
+        attn_energies = attn_energies.t()
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
 
 
 if __name__ == '__main__':
@@ -189,3 +343,11 @@ if __name__ == '__main__':
     # Print a sample of lines
     print("\nSample lines from file:")
     print_lines(datafile)
+
+    # Load and assemble voc and pairs
+    save_dir = os.path.join("data", "save")
+    voc, pairs = load_prepare_data(corpus, corpus_name, datafile, save_dir)
+    print("\npairs:")
+    for pair in pairs[:10]:
+        print(pair)
+    pairs = trim_rare_words(voc, pairs, MIN_COUNT)
